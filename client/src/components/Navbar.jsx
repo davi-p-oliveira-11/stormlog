7436e008405ed472e0330e221013d98b27300468
Davi P. Oliveira <dpo.dev11@gmail.com>
2026-02-04 15:44:28 -0300

    chore(init): scaffold project and install Tailwind CSS

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+const Navbar = () => {
+  return (
+    <div className="">
+      <div className="">
+        <h2>Stormblog</h2>
+      </div>
+      <div className="md:hidden">M</div>
+      <div className="hidden md:flex">D</div>
+    </div>
+  )
+}
+
+export default Navbar
